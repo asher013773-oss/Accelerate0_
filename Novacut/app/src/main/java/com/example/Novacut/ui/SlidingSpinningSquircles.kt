cc5480af30b9acc1d49197297b582511cec545f3
asher013773-oss <asher013773@gmail.com>
2026-08-21 12:23:02 +0100

    Remove unused import for spacedBy
    Removed unused import for spacedBy from SlidingSpinningSquircles.kt

--- Novacut/app/src/main/java/com/example/Novacut/ui/SlidingSpinningSquircles.kt
+++ Novacut/app/src/main/java/com/example/Novacut/ui/SlidingSpinningSquircles.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.sizeIn
-import androidx.compose.foundation.layout.spacedBy
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
